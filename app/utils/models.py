174e49bd7e1ae3c6351dc21e5cb41c033592b48a
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2024-10-22 10:36:22 +0000

    feat: Add models, default value for Category; Todo: Use with database

--- app/utils/models.py
+++ app/utils/models.py
@@ -7,7 +7,11 @@ class CategoryExample(BaseModel):
     code: str
 
 class Category(BaseModel):
+    id: int = 0
     name: str
     description: str
     examples: List[CategoryExample]
+    
+class DeleteCategory(BaseModel):
+    id: int
     
