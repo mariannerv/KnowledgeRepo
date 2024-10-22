2b87b47d9c53fe85d512cb57125cfd69772c80d0
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2024-10-22 10:36:53 +0000

    feat: Add delete, add endpoints

--- app/routers/data_manager.py
+++ app/routers/data_manager.py
@@ -1,10 +1,20 @@
 from fastapi import APIRouter, Request
-from app import load_knowledge_data
+from app import load_knowledge_data, Category, DeleteCategory
 
 router = APIRouter()
 
 
-@router.post("/load_knowledge_data", tags=["Knowledge Repo"])
+@router.post("/load_knowledge_data", tags=["Knowledge"])
 async def load_data(request: Request):
     dataset = load_knowledge_data()
     return dataset
+
+@router.post("/add_category", tags=["Knowledge"])
+async def add_category(payload: Category):
+    print(f"Received payload for category {payload.name}")
+    return 200
+
+@router.delete("/delete_category", tags=["Knowledge"])
+async def dlete_category(payload: DeleteCategory):
+    print(f"Deleting category {payload.id}")
+    return 200
